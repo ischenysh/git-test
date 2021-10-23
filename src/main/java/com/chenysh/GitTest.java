0b49c256c7ca7cfa18b101980d82ec0d2f163213
ischenysh <93030374+ischenysh@users.noreply.github.com>
2021-10-23 20:18:11 +0800

    222

--- src/main/java/com/chenysh/GitTest.java
+++ src/main/java/com/chenysh/GitTest.java
@@ -3,6 +3,7 @@ package com.chenysh;
 public class GitTest {
   public static void main(String[] args) {
     System.out.printf("Hello, git!");
+    System.out.printf("Hello, git!");
 //     in github
   }
 }
